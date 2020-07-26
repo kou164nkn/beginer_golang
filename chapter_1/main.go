7a78e9ea7444617d9ad15ab7eae24311cfe0cee1
kou164nkn <galahad1005@gmail.com>
2020-07-26 22:21:46 +0900

    インデントの崩れを修正

--- chapter_1/main.go
+++ chapter_1/main.go
@@ -4,23 +4,23 @@ package main
 import "fmt"
 
 func main() {
-	// println("コメントアウト")
+  // println("コメントアウト")
 
-	/*
-		ブロックコメント
-	*/
+  /*
+    ブロックコメント
+  */
 
-	// fmtパッケージの標準出力関数を呼び出す
-	fmt.Print("Hello, ") // 改行されない
-	fmt.Println("World") // 改行される
-	fmt.Println("A", 100, true, 1.5) // スペース区切りで出力
+  // fmtパッケージの標準出力関数を呼び出す
+  fmt.Print("Hello, ") // 改行されない
+  fmt.Println("World") // 改行される
+  fmt.Println("A", 100, true, 1.5) // スペース区切りで出力
 
-	// 書式を指定して標準出力を行う
-	fmt.Printf("Hello, World\n")     // \nで改行する
-	fmt.Printf("%d-%s", 100, "偶数\n") // %dは整数、%sは文字列
+  // 書式を指定して標準出力を行う
+  fmt.Printf("Hello, World\n")     // \nで改行する
+  fmt.Printf("%d-%s", 100, "偶数\n") // %dは整数、%sは文字列
 
-	var price int
-	fmt.Print("値段>")
-	fmt.Scan(&price)
-	fmt.Printf("%d円\n", price)
+  var price int
+  fmt.Print("値段>")
+  fmt.Scan(&price)
+  fmt.Printf("%d円\n", price)
 }
--- chapter_5/mycat.go
+++ chapter_5/mycat.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"flag"
-	"bufio"
-	"path/filepath"
+  "fmt"
+  "os"
+  "flag"
+  "bufio"
+  "path/filepath"
 )
 
 var line_flag bool
 var count int = 1
 
 func init() {
-	flag.BoolVar(&line_flag, "n", false, "flag line number")
+  flag.BoolVar(&line_flag, "n", false, "flag line number")
 }
 
 func main() {
-	flag.Parse()
-	files := flag.Args()
+  flag.Parse()
+  files := flag.Args()
 
-	for _, file := range files {
-		search_file(file)
-	}
+  for _, file := range files {
+    search_file(file)
+  }
 }
 
 func search_file(file string) {
-	err := filepath.Walk("/beginer_golang/chapter_5/",
-											 func(path string, info os.FileInfo, err error) error {
-													if filepath.Base(path) == file {
-														print_file(file)
-													}
-													return nil
-												 })
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "検索に失敗しました", err)
-	}
+  err := filepath.Walk("/beginer_golang/chapter_5/",
+                       func(path string, info os.FileInfo, err error) error {
+                         if filepath.Base(path) == file {
+                           print_file(file)
+                         }
+                           return nil
+                         })
+  if err != nil {
+    fmt.Fprintln(os.Stderr, "検索に失敗しました", err)
+  }
 }
 
 func print_file(file string) {
-	sf, err := os.Open(file)
+  sf, err := os.Open(file)
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "読み込みに失敗しました: ", err)
-		os.Exit(1)
-	}
+  if err != nil {
+    fmt.Fprintln(os.Stderr, "読み込みに失敗しました: ", err)
+    os.Exit(1)
+  }
 
-	src := bufio.NewScanner(sf)
-	for src.Scan() {
-		if line_flag {
-			fmt.Printf("%d: ", count)
-			count++
-		}
-		fmt.Fprintln(os.Stdout, src.Text())
-	}
+  src := bufio.NewScanner(sf)
+  for src.Scan() {
+    if line_flag {
+      fmt.Printf("%d: ", count)
+      count++
+    }
+    fmt.Fprintln(os.Stdout, src.Text())
+  }
 
-	defer sf.Close()
+  defer sf.Close()
 }
